6a5e29da2983feb4f78052101b8b0804a0419a2d
Carlos Urquiza <webquiza@gmail.com>
2021-01-26 20:36:15 -0800

    add working submit button that displays answers
    Co-authored-by: April Soetarman <aprilsoetarman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
     const beverage = $("#beverage").val();
-    alert(beverage);
     const meal = $("input:radio[name=meal]:checked").val();
     const dob = $("#born").val();
     const favoriteColor = $("#color").val(); 
 
-    $("#output").text(result);
+    $("#output1").text(beverage);
+    $("#output2").text(meal);
+    $("#output3").text(dob);
+    $("#output4").text(favoriteColor);
   });
 });
